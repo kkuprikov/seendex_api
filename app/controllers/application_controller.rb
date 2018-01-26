8e32b277f895a6fc336e31c9e6f9b9ae7339c832
Kirill Kuprikov <kkuprikov@gmail.com>
2018-01-26 15:55:05 +0300

    Removed error payload

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,2 @@
 class ApplicationController < ActionController::API
-  protected
-
-  def render_error_payload(identifier, status: :bad_request)
-    render json: ErrorPayload.new(identifier, status), status: status
-  end
 end
